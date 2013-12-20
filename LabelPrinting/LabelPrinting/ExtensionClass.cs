27261972f73186fc14b020699dd56ca13a85b042
unknown <ali.koshar@iris-csg-269.irissoftware.com>
2013-12-20 16:46:18 +0530

    Label Printing Service Updated

--- LabelPrinting/LabelPrinting/ExtensionClass.cs
+++ LabelPrinting/LabelPrinting/ExtensionClass.cs
@@ -4,8 +4,7 @@ using System.IO;
 namespace LabelPrinting
 {
     public static class ExtensionClass
-    {
-       
+    {       
         public static Image ToImage(this byte[] byteArrayIn)
         {
             Image returnImage = null;
@@ -24,15 +23,5 @@ namespace LabelPrinting
             return ms.ToArray();
         }
 
-        public static void WriteToFileStream(this Stream input, FileStream output)
-        {
-            byte[] buffer = new byte[8 * 1024];
-            int len;
-            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                output.Write(buffer, 0, len);
-            }
-            output.Close();
-        }
     }
 }
